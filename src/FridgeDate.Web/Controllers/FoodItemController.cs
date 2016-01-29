45a88763b2bcdb318e2a27176da27fe771eef90b
Arnstein Johansen <arnstej@gmail.com>
2016-01-29 10:22:29 +0100

    Added create view for food items

---
+++ src/FridgeDate.Web/Controllers/FoodItemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FridgeDate.Web.Controllers
+{
+    public class FoodItemController : Controller
+    {
+        // GET: FoodItem
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/FridgeDate.Web/Models/FoodItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using FridgeDate.Core.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace FridgeDate.Web.Models
+{
+    public class FoodItemViewModel
+    {
+        [Display(Name = "Product name")]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Shelflife in days")]
+        public int ShelfLifeDays { get; set; }
+
+        [Display(Name = "Barcode id")]
+        public int BarCodeId { get; set; }
+
+        //public int MyProperty { get; set; }
+    }
+}
