2924764270a834ebdc60cec743be9ca716d948e0
Arnstein Johansen <arnstej@gmail.com>
2016-02-28 11:27:02 +0100

    Removed password requirements

--- src/FridgeDate.API/App_Start/IdentityConfig.cs
+++ src/FridgeDate.API/App_Start/IdentityConfig.cs
@@ -27,11 +27,7 @@ namespace FridgeDate.API
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 6
             };
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
--- src/FridgeDate.API/App_Start/Startup.Auth.cs
+++ src/FridgeDate.API/App_Start/Startup.Auth.cs
@@ -40,7 +40,7 @@ namespace FridgeDate.API
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
                 AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
                 // In production mode set AllowInsecureHttp = false
-                AllowInsecureHttp = true
+                AllowInsecureHttp = true,
             };
 
             // Enable the application to use bearer tokens to authenticate users
