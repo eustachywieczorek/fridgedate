db45b08f096b9f6f0ce19d8e3b7bb2c80323b532
Arnstein Johansen <arnstej@gmail.com>
2016-03-14 08:57:47 +0100

    Added a base controller for api

---
+++ src/FridgeDate.API/Controllers/BaseApiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using FridgeDate.Data.Interfaces;
+
+namespace FridgeDate.API.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        protected IMapper Mapper;
+        protected IUnitOfWork UnitOfWork;
+        public BaseApiController(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            Mapper = mapper;
+            UnitOfWork = unitOfWork;
+        }
+
+    }
+}
